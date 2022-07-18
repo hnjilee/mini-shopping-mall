85e2378d3ad026a795f6599fd2d59f01f6585491
hnjilee <hnjilee@gmail.com>
2022-07-18 17:28:51 +0900

    Sort products by price on select change

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ loadProducts() //
     // Filter products by category on button click
     const btns = document.querySelector('.btns');
     btns.addEventListener('click', e => onBtnclick(e, productElements));
+
+    // Sort products by price on select change
+    const select = document.querySelector('.select');
+    select.addEventListener('change', e => onSelectChange(e, productElements));
   })
   .catch(console.log);
 
@@ -31,6 +35,7 @@ function createElement(product) {
   const listItem = document.createElement('li');
   listItem.classList.add('product');
   listItem.setAttribute('data-category', product.category);
+  listItem.setAttribute('data-price', product.price);
   listItem.innerHTML = `
     <img src=${product.img} alt=${product.name} class="product__img" />
     <p class="product__price">${product.price}</p>
@@ -54,4 +59,29 @@ function filterProducts(products, category) {
       product.classList.add('product--hidden');
     }
   });
+}
+
+function onSelectChange(e, products) {
+  const criteria = e.target.value;
+  sortProducts(products, criteria);
+}
+
+function sortProducts(products, criteria) {
+  const toBeSorted = [...products];
+  let sorted;
+
+  switch (criteria) {
+    case 'ascending':
+      sorted = toBeSorted.sort((a, b) => a.dataset.price - b.dataset.price);
+      break;
+    case 'descending':
+      sorted = toBeSorted.sort((a, b) => b.dataset.price - a.dataset.price);
+      break;
+    case '':
+      sorted = products;
+      break;
+  }
+
+  const container = document.querySelector('.products');
+  container.append(...sorted);
 }
