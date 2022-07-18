9d3034b08bef19d5631fb60e35710f821d81ae65
hnjilee <hnjilee@gmail.com>
2022-07-18 16:58:56 +0900

    Filter products by category on btn click

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 loadProducts() //
   .then(products => {
-    displayProducts(products);
+    const productElements = displayProducts(products);
+
+    // Filter products by category on button click
+    const btns = document.querySelector('.btns');
+    btns.addEventListener('click', e => onBtnclick(e, productElements));
   })
   .catch(console.log);
 
@@ -20,14 +24,34 @@ function displayProducts(products) {
   const container = document.querySelector('.products');
   const elements = products.map(createElement);
   container.append(...elements);
+  return elements;
 }
 
 function createElement(product) {
   const listItem = document.createElement('li');
   listItem.classList.add('product');
+  listItem.setAttribute('data-category', product.category);
   listItem.innerHTML = `
     <img src=${product.img} alt=${product.name} class="product__img" />
     <p class="product__price">${product.price}</p>
     `;
   return listItem;
+}
+
+function onBtnclick(e, products) {
+  const category = e.target.dataset.category;
+  if (!category) {
+    return;
+  }
+  filterProducts(products, category);
+}
+
+function filterProducts(products, category) {
+  products.forEach(product => {
+    if (category === '*' || category === product.dataset.category) {
+      product.classList.remove('product--hidden');
+    } else {
+      product.classList.add('product--hidden');
+    }
+  });
 }
